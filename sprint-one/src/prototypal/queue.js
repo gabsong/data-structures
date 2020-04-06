6b6d71717372fbcabc469b77c9234729b5cf4b77
Gabriel Song <gabsong4@gmail.com>
2020-04-06 15:55:28 -0700

    Implement stack & queue in prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,36 @@
+'use strict';
+
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+
+  queue._storage = {};
+  queue._firstIndex = 0;
+  queue._nextIndex = 0;
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (value) {
+    this._storage[this._nextIndex] = value;
+    this._nextIndex += 1;
+    return this.size();
+  },
+
+  dequeue: function () {
+    if (this.size() > 0) {
+      var removedValue = this._storage[this._firstIndex];
+      delete this._storage[this._firstIndex];
+      this._firstIndex += 1;
+      return removedValue;
+    } else {
+      return;
+    }
+  },
+
+  size: function () {
+    return this._nextIndex - this._firstIndex;
+  },
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
+'use strict';
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+
+  stack._storage = {};
+  stack._nextIndex = 0;
+
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value) {
+    this._storage[this._nextIndex] = value;
+    this._nextIndex += 1;
+    return this.size();
+  },
 
+  pop: function () {
+    if (this.size() > 0) {
+      var removedValue = this._storage[this._nextIndex - 1];
+      delete this._storage[this._nextIndex - 1];
+      this._nextIndex -= 1;
+      return removedValue;
+    } else {
+      return;
+    }
+  },
 
+  size: function () {
+    return this._nextIndex;
+  },
+};
