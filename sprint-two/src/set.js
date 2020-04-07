304ec80d04051d570b2e9acaa558515e8ce44127
Gabriel Song <gabsong4@gmail.com>
2020-04-06 17:46:45 -0700

    Implement set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,35 @@
+'use strict';
+
+// Prototypal class instantiation pattern
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    // we only add a value to a set once
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  // to lookup by value, key == value
+  return this._storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add = constant time, O(1)
+ * remove = constant time, O(1)
+ * contains = constant time, O(1)
  */
